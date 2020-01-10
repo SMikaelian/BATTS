85e905a33ca3d6a4259a50dd4c964ae93598463c
Nikolaos Stroubos <nikoumwelt@gmail.com>
2020-01-10 10:02:10 -0800

    Updated README.md

--- Ground_Station/Ground_Station.ino
+++ Ground_Station/Ground_Station.ino
@@ -1,37 +1,57 @@
- float arrRPY[3];
+// Code to receive serial floating point sensor data - Dylan Reynolds, Cape Town, South Africa
+// I take no responsibility for accidents resulting in the use of this code. This is 
+// merely a guideline and free for modification.
+//
+// NOTES:
+// - I was using this to transmit the roll pitch and yaw from and IMU to a ground station, I
+//   am sure it can be adapted to applications such as GPS coordinates, temperature etc.
+//
+// SETUP:
+//  ________________________            _______________________
+// |       TRANSMITTER      |          |        RECEIVER       |
+// | Teensy 3.0 + MPU6050 + | -------> | Arduino Leonardo R3 + |
+// | 3DR  915MHz Air Module | -------> | 3DR 915MHz Air Module |
+// |________________________|          |_______________________|
+//
+// - Print serial data normally on the sending side as if you were going to send it serially
+//   over the USB port (Serial1 for hardware pins on Leonardo etc.)
+// - Use the following code on the receiving controller:
 
-void setup()
-{
-  Serial.begin(9600);
+float arrRPY[9];                                   // Array to store the roll, pitch & yaw values
+
+void setup() {                                     // Setup function that runs initially on MCU startup
+  Serial.begin(57600);                             // Start serial usb connection
+  Serial3.begin(57600);                            // Start hardware serial pins connection
 }
 
-void loop()
-{
-  
-}
+void getRPY();
 
-void getRPY() {     // Get roll, pitch & yaw function
-  byte byteRead;   // Read first byte of the incoming transmission
-  boolean startByte = false;   // Boolean opperator
+void loop() {                                      // Loop function running continuously after MCU has been setup
+//  getRPY();                                        // Function storing roll, pitch & yaw in arrRPY array
+//  Serial.println("This is void loop");
+  delay(500);
+  byte byteRead;                                   // Serial comms transmits bytes, this stores the initially read byte from the serial stream
 
-  
-  while (Serial.available() > 0) {  // While there is serial data being trasmitted
-    
-    byteRead = Serial.read();  // Read first byte received
-    Serial.println(byteRead);   // Visual check for myself to see what it is
-    
-    while (byteRead >= 60 || byteRead <= 60) {  // 60 is ASCII code for <, which is my identifier
-      byteRead = Serial.read();   // read next byte until it is 60 (<)
-      Serial.println(byteRead);    // Visual check for myself
+  while(Serial2.available() || Serial1.available() || Serial.available()) {
+    Serial.println("Serial2 Available!");
     }
-    if (byteRead == 60) {  // If 60 (<) is detected
-      startByte = true;       // Change boolean operator value
-    if (startByte) {             // If startByte is detected use Serial.parseFloat() to detect the next floating
-      arrRPY[0] = Serial.parseFloat();  //point value in the serial communication and write the floating
-      arrRPY[1] = Serial.parseFloat();  // point value to the storage array.
-      arrRPY[2] = Serial.parseFloat();
+
+
+  while (Serial3.available() > 0) {                // While data is available in the serial buffer
+
+
+    byteRead = Serial3.read();                     // Read the first byte in the serial buffer
+    // Searching for the start byte "<" (ASCII = 60) 
+    while (byteRead > 60 || byteRead < 60) {       // If the byte value is greater than or less that 60
+      byteRead = Serial1.read();                   // keep reading the byte stream until 60 is found, ie. "<"
     }
-  }
-  Serial.print(arrRPY[0]); Serial.print(" | "); Serial.print(arrRPY[1]); Serial.print(" | "); Serial.println(arrRPY[2]);  // Visual check to see what has been written to the array.
+    if (byteRead == 60) {                          // If "<" found
+      for (int i = 1 ; i <= 9 ; i++) {             // I expected 3 values, so I looped 3 times
+        arrRPY[i-1] = Serial3.parseFloat();          // Write the parsed floating point value found in the serial stream to arrRPY
+      }
+    }
+    // Check that the correct values are being written to the array by examining them in a serial terminal
+    Serial.print("<"); Serial.print(arrRPY[0]); Serial.print(","); Serial.print(arrRPY[1]); Serial.print(","); Serial.print(arrRPY[2]); Serial.println(">");
+    break;
   }
 }
--- README.md
+++ README.md
@@ -2,8 +2,40 @@
 Ballistic Acquisition Telemetry and Trajectory System using an Arduino to report telemetry from a self-contained device such as a drone to a fixed ground station.
 
 
-#### Questions:
-* What communication protocol should be used by each device?
-* What is the range of the radios being used?
-* What devices are being used?
-	- Currently: Arduino Mega 2560, Teensy 3.2, 3R 915MHz radios, and the MPU-9250 board.
+Questions:
+-How should each axis of data be mapped?
+-What communication protocol is each component of the project using?
+-How will the data be collected from the Arduino?
+-How does the Beitan GPS work? How do we print data from it to the serial monitor repeatedly?
+-How does the Serial*.available() function work?
+-How many devices can the Mega handle?
+-At what baud rate is the data optimally received at from each device?
+-What have we learned so far?
+-If we were to receive data from the computer, how would it be graphed?
+
+
+Priority I:
+-Transmission of data from the Teensy 3.2 to an Arduino using the 3DR radio transmitter/receiver.
+
+
+Priority II:
+-Read data from Accelerometer board to Teensy, document and implement a simple data transmission protocol. Eg. A = Accelerometer Data, G = Gyroscope Data, M = MPU Magnetometer Data, and  AD = ADXL345 Board Data
+
+Data is output to the serial monitor according to the same scheme it’s sent to the radio transmitter. Eg.
+
+< A.x, A.y, A.z | G.x, G.y, G.z | M.x, M.y, Mz | AD.x, AD.y, AD.z >
+
+The characters ‘<‘ and ‘>’ have ASCII values 60 and 62, which can be detected when parsing the data. If there is a loss of some of the data.
+
+The way I wrote the above example assumes that the data from the MPU and the magnetometer board would transmit data in sync. There is no reason to believe this would be the case. What are the implications of this?
+
+Priority III:
+-Create a case to house the electronics. Power the circuit with a 5V LiPo battery and a basic Adafruit battery charger board.
+
+
+
+#### 1.8.20
+
+9 Axes of data are transmitted from the Teensy wirelessly using BATTS/Mobile_Station/Mobile_Station_V2/Mobile_Station_V2.ino. These are all being read via I2C from the MPU-9250 accelerometer/gyro/compass board. The format looks like `< A.x, A.y, A.z, G.x, G.y, G.z, M.x, M.y, Mz >`.
+
+We are having trouble reading any data from the ground station. We were using code from [this guy's project](https://forum.arduino.cc/index.php?topic=307172.0).
