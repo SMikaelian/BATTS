e0e033434c2d00eda93582562d2b5ebece3d62d4
SMikaelian <sarkis.mikaelian.712@my.csun.edu>
2020-01-24 19:51:52 -0800

    Possible fixes to the ground station.

--- src/Ground_Station/Ground_Station.ino
+++ src/Ground_Station/Ground_Station.ino
@@ -64,7 +64,7 @@
 //   over the USB port (Serial2 for hardware pins on Leonardo etc.)
 // - Use the following code on the receiving controller:
 
-float arrRPY[3];                                   // Array to store the roll, pitch & yaw values
+float arrRPY[100];                                   // Array to store the roll, pitch & yaw values
 
 void setup() {                                     // Setup function that runs initially on MCU startup
   Serial.begin(57600);                             // Start serial usb connection
@@ -76,22 +76,15 @@ void loop() {                                      // Loop function running cont
 }
 
 void getRPY() {                                    // Function storing roll, pitch & yaw in arrRPY array
-  byte byteRead;                                   // Serial comms transmits bytes, this stores the initially read byte from the serial stream
-  if(Serial2.available()){
-    Serial.println("!Serial2");
-  } else if (!Serial2.available() > 0) {
-    Serial.println("Serial2.available");
-  }
+  byte byteRead;                                   // Serial comms transmits bytes, this stores the initially read byte from the serial strea
   while (Serial2.available() > 0) {                // While data is available in the serial buffer
     Serial.println("Serial2 is available");
     byteRead = Serial2.read();                     // Read the first byte in the serial buffer
-    // Searching for the start byte "<" (ASCII = 60)
-      // If the byte value is greater than or less that 60
-      byteRead = Serial2.read();                   // keep reading the byte stream until 60 is found, ie. "<"
-
-//        arrRPY[i] = Serial2.parseFloat();          // Write the parsed floating point value found in the serial stream to arrRPY
-//        Serial.print("Serial2.parseFLoat()");
-    // Check that the correct values are being written to the array by examining them in a serial terminal
+    byteRead = Serial2.read();                   // keep reading the byte stream until 60 is found, ie. "<"
+    for (int i = 0 ; i < 100 ; i++) {             // I expected 3 values, so I looped 3 times
+      arrRPY[i] = Serial2.parseFloat();          // Write the parsed floating point value found in the serial stream to arrRPY
+      Serial.print("Serial2.parseFLoat()");
+    }
     Serial.print("<"); Serial.print(arrRPY[0]); Serial.print(","); Serial.print(arrRPY[1]); Serial.print(","); Serial.print(arrRPY[2]); Serial.println(">");
-      }
+    }
 }
